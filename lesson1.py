b6ac9b6ea855512f85ca997ba767fc158a317a4a
Stefanos Zisidis <stefanos.zisidis@stcatherines.gr>
2026-09-15 09:52:30 +0300

    Intro files

---
+++ lesson1.py
@@ -0,0 +1,11 @@
+# Mr.Robot
+# Virus to destroy all Databases
+# version: 0.1
+# last_update: 08.09.2026
+
+# print("hello world")
+# print("Welcome to IB CS!")
+# # STart of the week: monday
+# print("fgfsdgdfgsdfgdfg")
+
+print(102)
---
+++ lesson2_14.09.2026.py
@@ -0,0 +1,26 @@
+# Mr.Robot
+#
+# version: 0.1
+# last_update: 14.09.2026
+
+# Please write a program which prints out the number of minutes in a year.
+# Use Python code to perform the calculation, as in the previous code example.
+# print("number of minutes in a year...")
+# print((60*24*365))
+#
+# print("print(\"Hello There!\")")
+# print('print("Hello There!")')
+
+name = input("What is your name?")
+
+print(name,"\t",name)
+print("Hello\nevery\tone")
+
+
+# print(type(name))
+
+# number1 = int(input("Type a number: "))
+# number2 = int(input("Type another number: "))
+# print(type(number1))
+#
+# print(number1 % number2)
