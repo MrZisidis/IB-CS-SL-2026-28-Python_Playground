ac612f0b9e36df149e799087216de4491302d39f
Stefanos Zisidis <stefanos.zisidis@stcatherines.gr>
2026-09-16 12:25:36 +0300

    Intro files

---
+++ lessson3_16.09.2026.py
@@ -0,0 +1,25 @@
+# number1 = int(input("Type a number: "))
+# number2 = int(input("Type another number: "))
+# print(number1,"\t",number2)
+# print(f"{number1}\t+\t{number2} = {number1 + number2}")
+# print("Title\t\t","Name\t\t")
+# print("_"*45)
+
+
+name = "Tim Tester"
+age = 20
+skill1 = "python"
+level1 = "beginner"
+skill2 = "java"
+level2 = "veteran"
+skill3 = "programming"
+level3 = "semiprofessional"
+lower = 2000
+upper = 3000
+
+print("my name is ", name, " , I am ", age, "years old")
+print("my skills are")
+print("- ", skill1, " (", level1, ")")
+print("- ", skill2, " (", level2, ")")
+print("- ", skill3, " (", level3, " )")
+print("I am looking for a job with a salary of", lower, "-", upper, "euros per month")
