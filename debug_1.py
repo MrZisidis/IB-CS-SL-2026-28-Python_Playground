5743d7e74a3112ed2e8a9063dcf214f7465b1b93
Stefanos Zisidis <stefanos.zisidis@stcatherines.gr>
2026-09-23 13:30:39 +0300

    debugging lesson

---
+++ debug_1.py
@@ -0,0 +1,34 @@
+# item_count = 5
+# shipping_total = 50.0
+# discount_applied = False
+#
+# # Debugging: check the state before the condition
+# print(f"Debug: item_count = {item_count}")    #print("Debug: item_count =", item_count)
+#
+# if item_count > 5:
+#     print("inside my selection")
+#     shipping_total = shipping_total * 0.8  # 20% discount
+#     discount_applied = True
+# print(" sdfsdfsd")
+# item_count = item_count + 1
+#
+# # Debugging: check the state after the condition
+# print(f"Debug: item_count = {item_count}")
+# print(f"Debug: discount_applied = {discount_applied}")
+
+# c= 10
+# if c ==10:
+    raise Exception("We don't like tens around here.")
+
+# print("Hello world!")
+# raise Exception("I don't want to work today")
+# print("end of the program")
+
+# x = 4/0
+print("Hello, World!")
+try:
+    x = 4 / 0
+except:
+    print("there was an error")
+
+print("End of the program")
